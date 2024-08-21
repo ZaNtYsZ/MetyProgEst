e76f637351ed41ea5b5b5b4988b4c43fb1c7199e
ZaNtYsZ <ssmena@uamv.edu.ni>
2024-08-21 11:07:03 -0600

    primeros ejercicios C#

---
+++ ejercicio2/ejercicio2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            Console.WriteLine("Escribe tu primer nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre);
+        }
+    }
+}
