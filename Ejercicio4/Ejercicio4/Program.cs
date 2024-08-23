5e97cd7cdd9f6015ac4edf134b04909bf492d4f9
ZaNtYsZ <ssmena@uamv.edu.ni>
2024-08-22 18:15:02 -0600

    ain

---
+++ Ejercicio4/Ejercicio4/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Ejercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Ingrese el radio de un circulo: ");
+            double radio = double.Parse(Console.ReadLine());
+            double area = Math.PI * Math.Pow(radio, 2);
+         
+            Console.WriteLine("El area calculado del circulo con radio {0} es: {1}", radio, area);
+            
+        }
+
+    }
+}
