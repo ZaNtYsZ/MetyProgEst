14fd26865b6e117d9900ec13dd551a14374f116c
ZaNtYsZ <ssmena@uamv.edu.ni>
2024-08-22 20:09:07 -0600

    ejercicio7

---
+++ Ejercicio6/Ejercicio7/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ejercicio7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escriba un numero entero: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("El numero {0} es par...", numero);
+            }
+
+            else
+            {
+                Console.WriteLine("El numero {0} es impar...", numero);
+            }
+
+        }
+    }
+}
