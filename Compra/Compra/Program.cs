480980d9cde8228e52272f9cb630dabe4071b0de
ZaNtYsZ <ssmena@uamv.edu.ni>
2024-08-23 08:41:28 -0600

    clase 23/08

---
+++ Compra/Compra/Program.cs
@@ -0,0 +1,36 @@
+﻿using Compra.clases;
+using System.Runtime.CompilerServices;
+
+namespace Compra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejemplo 1");
+            Factura fact = new Factura();
+            string producto= null;
+            double precio=0, monto;
+            int cantidad=0;
+            Console.Write("Producto: ");
+            producto = Console.ReadLine();
+            Console.Write("Precio: ");
+            precio = double.Parse(Console.ReadLine());
+            Console.Write("Cantidad: ");
+            cantidad = int.Parse(Console.ReadLine());
+
+            fact.producto = producto;
+            fact.precio = precio;   
+            fact.cantidad = cantidad;
+
+            monto = fact.calcularMonto();       
+
+
+            Console.WriteLine($"{producto}");
+            Console.WriteLine($"{precio}");
+            Console.WriteLine($"{cantidad}");
+            Console.WriteLine($"{monto}");  
+
+        }
+    }
+}
---
+++ Compra/Compra/clases/Factura.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compra.clases
+{
+    internal class Factura
+    {
+        public string producto { get; set; }
+        public double precio { get; set; }
+        public int cantidad { get; set; }
+        public double calcularMonto()
+        {
+            return this.precio * this.cantidad;
+        }
+
+        public Factura() { }
+    }
+}
