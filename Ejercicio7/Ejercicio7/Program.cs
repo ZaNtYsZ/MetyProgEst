e9cb5818de8b63aced55ec2b2edd77422d0d2f09
ZaNtYsZ <ssmena@uamv.edu.ni>
2024-08-22 21:00:31 -0600

    ejer7

---
+++ Ejercicio7/Ejercicio7/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Ejercicio7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ///Escribe un programa que lea tres números enteros y determine cuál de ellos es el mayor.
+            
+            Console.Write("Ingrese el primer numero: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el segundo número: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el tercer número: ");
+            int num3 = int.Parse(Console.ReadLine());
+
+            int mayor;
+
+            if (num1 > num2 && num1 > num3)
+            {
+                mayor = num1;
+            }
+
+            else if (num2 > num1 && num2 > num3)
+            {
+                mayor = num2;
+            }
+
+            else
+            {
+                mayor = num3;
+            }
+
+            Console.WriteLine("El numero mayor es: " + mayor);
+        }
+    }
+}
