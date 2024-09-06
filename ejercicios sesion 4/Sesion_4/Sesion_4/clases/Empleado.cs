364c68fecfd0e4941c797f6bac8099e2dce1b79d
Santiago Mena <ssmena@uamv.edu.ni>
2024-09-05 20:29:34 -0600

    sesion4

---
+++ ejercicios sesion 4/Sesion_4/Sesion_4/clases/Empleado.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sesion_4.clases
+{
+    //Calcular el salario neto de un empleado 
+    internal class Empleado
+    {
+        public Empleado() { }
+
+        public int ID { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public double Phone { get; set; }
+        public double Salary { get; set; }
+
+        public Empleado(int iD, string firstname, string lastname, string email, double phone, double salary)
+        {
+            ID = iD;
+            Firstname = firstname;
+            Lastname = lastname;
+            Email = email;
+            Phone = phone;
+            Salary = salary;
+        }
+    }
+}
---
+++ ejercicios sesion 4/Sesion_4/Sesion_4/clases/RRHH.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sesion_4.clases
+{
+    internal class RRHH
+    {
+        //Calcular el salario neto de un empleado
+        public RRHH() { }
+        public CalcularSalario(Empleado empleado, double deducciones)
+        {
+            return empleado.Salary - deducciones;
+        }
+
+        //Aumentar el salario de un empleado en un 5% si su salario es menor a x salario
+        public void AumentarSalario(Empleado empleado, double porcentaje, double techo)
+        {
+            empleado.Salary = (empleado.Salary <= techo) ? empleado.Salary = (empleado.Salary * (1 + porcentaje)) : empleado.Salary;
+        }
+        
+    }
+}
